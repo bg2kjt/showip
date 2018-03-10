7720bc851ffa3066b5c4d1c9f316269cbd67dfbf
bg2kjt <34206455+bg2kjt@users.noreply.github.com>
2018-03-10 20:28:27 +0800

    Update nano.sh

--- nano.sh
+++ nano.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo mount -o remount,rw /
 #download shopip file
-sudo cd/usr/local/bin
-wget https://github.com/bg2kjt/showip/master/nanopi/MMDVMHost-ip
+sudo cd /usr/local/bin
+wget https://github.com/bg2kjt/showip/master/raw/nanopi/MMDVMHost-ip
 sudo cp MMDVMHost MMDVMHost-o
 sudo rm MMDVMHost
 sudo cp MMDVMHost-ip MMDVMHost
@@ -11,5 +11,5 @@ sudo chmod 755 MMDVMHost
 sudo cd /etc
 sudo cp mmdvmhost mmdvmhost-o
 sudo rm mmdvmhost
-wget https://github.com/bg2kjt/showip/master/mmdvmhost
+wget https://github.com/bg2kjt/showip/raw/master/mmdvmhost
 sudo mount -o remount,ro /
