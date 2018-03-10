f4154b4f6644c53285bfbf2de1b9b0e55e232673
bg2kjt <34206455+bg2kjt@users.noreply.github.com>
2018-03-10 21:01:08 +0800

    Update rasp.sh

--- rasp.sh
+++ rasp.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-sudo su -
-mount -o remount,rw /
+sudo mount -o remount,rw /
 #download shopip file
 cd /usr/local/bin
-wget https://github.com/bg2kjt/showip/raw/master/raspberry/MMDVMHost-ip
-cp MMDVMHost MMDVMHost-o
-rm MMDVMHost
-cp MMDVMHost-ip MMDVMHost
-chmod 755 MMDVMHost
+sudo wget https://github.com/bg2kjt/showip/raw/master/raspberry/MMDVMHost-ip
+sudo cp MMDVMHost MMDVMHost-o
+sudo rm MMDVMHost
+sudo cp MMDVMHost-ip MMDVMHost
+sudo chmod 755 MMDVMHost
 #download config file
 cd /etc
-cp mmdvmhost mmdvmhost-o
-rm mmdvmhost
-wget https://github.com/bg2kjt/showip/raw/master/mmdvmhost
-mount -o remount,ro /
+sudo cp mmdvmhost mmdvmhost-o
+sudo rm mmdvmhost
+sudo wget https://github.com/bg2kjt/showip/raw/master/mmdvmhost
+sudo mount -o remount,ro /
