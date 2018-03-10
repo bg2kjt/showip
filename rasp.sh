78ad6eea5be12afaa7ce67198732a78e7645161e
bg2kjt <34206455+bg2kjt@users.noreply.github.com>
2018-03-10 22:38:33 +0800

    Update rasp.sh

--- rasp.sh
+++ rasp.sh
@@ -12,5 +12,6 @@ cd /etc
 sudo cp mmdvmhost mmdvmhost-o
 sudo rm mmdvmhost
 sudo wget https://github.com/bg2kjt/showip/raw/master/mmdvmhost
+sudo rm /home/pi-star/rasp.sh
 sudo mount -o remount,ro /
 sudo reboot
