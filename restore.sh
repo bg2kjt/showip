61776e23e48f2becd5abba97f01b7cbb9a04cd3e
bg2kjt <34206455+bg2kjt@users.noreply.github.com>
2018-04-28 21:16:44 +0800

    Create restore.sh

---
+++ restore.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo mount -o remount,rw /
+#Restore the modified MMDVMHost
+cd /usr/local/bin
+sudo cp MMDVMHost-o MMDVMHost
+#Restore the modified mmdvmhost
+cd /etc
+sudo rm mmdvmhost
+sudo cp mmdvmhost-o mmdvmhost
+sudo rm /home/pi-star/restore.sh
+sudo mount -o remount,ro /
+#update&upgrade
+sudo pistar-update && pistar-upgrade
+#sudo reboot
